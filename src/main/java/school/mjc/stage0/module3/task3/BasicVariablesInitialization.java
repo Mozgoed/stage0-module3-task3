f5d699cde8d09596b8b41d89276414573b1ad201
Кононов Алексей <aleksey.n.kononov@gazprombank.ru>
2023-07-07 14:08:11 +0300

    done

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        System.out.println(first);
+        int second = 10;
+        System.out.println(second);
+        int third = 100;
+        System.out.println(third);
     }
 }
